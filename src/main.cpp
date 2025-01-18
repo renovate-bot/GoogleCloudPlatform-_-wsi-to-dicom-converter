1504a6afa6c83d596ee99df4cc94c99318ebda03
Philbrick <Kenneth.Philbrick@gmail.com>
2025-01-18 16:19:54 -0600

    update linter

--- src/main.cpp
+++ src/main.cpp
@@ -11,15 +11,15 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-#include <boost/program_options.hpp>
-
 #include <algorithm>
 #include <iostream>
 #include <string>
 #include <utility>
 #include <vector>
 
+#include <boost/program_options.hpp>
 #include "src/wsiToDcm.h"
+
 namespace {
 const size_t ERROR_IN_COMMAND_LINE = 1;
 const size_t SUCCESS = 0;
