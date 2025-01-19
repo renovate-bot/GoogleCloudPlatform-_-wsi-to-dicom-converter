d875ab3131386895fcfedd77313e608c84e233f5
Philbrick <Kenneth.Philbrick@gmail.com>
2025-01-18 20:43:46 -0600

    Update jpegUtil.cpp

--- src/jpegUtil.cpp
+++ src/jpegUtil.cpp
@@ -11,10 +11,10 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-#include <stdio.h>
 #include <stddef.h>
 #include <jpeglib.h>
 
+#include <cstdio>
 #include <csetjmp>
 #include <memory>
 #include <utility>
