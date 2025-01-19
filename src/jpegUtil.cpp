7aa155f752d16cf8cd77448ca7f21c860855180a
Philbrick <Kenneth.Philbrick@gmail.com>
2025-01-18 19:04:39 -0600

    Update jpegUtil.cpp

--- src/jpegUtil.cpp
+++ src/jpegUtil.cpp
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 #include <jpeglib.h>
+
+#include <cstddef>
 #include <csetjmp>
 #include <memory>
 #include <utility>
